5f11e1ac29d889d239e03f4b3ab12cdacd8571ba
Ryan Darley <spook6@gmail.com>
2012-03-22 12:51:55 -0500

    created project

---
+++ src/com/xv/cityorg/CityOrgActivity.java
@@ -0,0 +1,13 @@
+package com.xv.cityorg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CityOrgActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
